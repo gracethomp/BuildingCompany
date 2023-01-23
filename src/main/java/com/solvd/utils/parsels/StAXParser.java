f897a07d8c8816a2c4c0d33803182315312e0fbf
Olena Babii <gracetomlinson26@gmail.com>
2023-01-23 17:49:27 +0200

    StAX (logic) is done

--- src/main/java/com/solvd/utils/parsels/StAXParser.java
+++ src/main/java/com/solvd/utils/parsels/StAXParser.java
@@ -7,6 +7,7 @@ import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.events.Attribute;
+import javax.xml.stream.events.EndElement;
 import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
 import java.io.FileInputStream;
@@ -18,8 +19,10 @@ public class StAXParser {
     public static void doParse() {
         List<BuildingOrder> buildingOrders = new ArrayList<>();
         Building building = new Building();
+        Employee employee = new Employee();
         Client client = new Client();
         User user = new User();
+        StringBuilder stringBuilder = new StringBuilder();
         BuildingOrder buildingOrder = new BuildingOrder();
         XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
         try {
@@ -60,12 +63,54 @@ public class StAXParser {
                         case "type" -> building.setType(new BuildingType(Long.
                                 valueOf(startElement.getAttributeByName(new QName("id")).getValue()),
                                 xmlEventReader.nextEvent().asCharacters().getData()));
+                        case "manager" -> {
+                            employee = new Employee();
+                            employee.setId(Long.valueOf(startElement.
+                                    getAttributeByName(new QName("id")).getValue()));
+                            buildingOrder.setManager(employee);
+                        }
+                        case "department" -> {
+                            Department department = new Department();
+                            department.setDepartmentName(xmlEventReader.nextEvent().asCharacters().getData());
+                            department.setId(Long.valueOf(startElement.
+                                    getAttributeByName(new QName("id")).getValue()));
+                            employee.setDepartment(department);
+                        }
+                        case "status" -> buildingOrder.setStatus(new OrderStatus(Long.valueOf(startElement.
+                                getAttributeByName(new QName("id")).getValue()), xmlEventReader.
+                                nextEvent().asCharacters().getData()));
+                        default -> stringBuilder = new StringBuilder(xmlEventReader.
+                                nextEvent().asCharacters().getData());
+                    }
+                }
+                if(event.isEndElement()) {
+                    EndElement element = event.asEndElement();
+                    switch (element.getName().getLocalPart()) {
+                        case "user" -> client.setUser(user);
+                        case "email" -> user.setEmail(stringBuilder.toString());
+                        case "password" -> user.setPassword(stringBuilder.toString());
+                        case "fName" -> user.setFirstName(stringBuilder.toString());
+                        case "lName" -> user.setLastName(stringBuilder.toString());
+                        case "phoneNumber" -> user.setPhoneNumber(stringBuilder.toString());
+                        case "building" -> buildingOrder.setBuilding(building);
+                        case "buildingOrder" -> buildingOrders.add(buildingOrder);
+                        case "buildingOrders" -> client.setBuildingOrders(buildingOrders);
+                        case "area" -> building.setArea(Double.valueOf(stringBuilder.toString()));
+                        case "floors" -> building.setFloors(Integer.valueOf(stringBuilder.toString()));
+                        case "address" -> building.setAddress(stringBuilder.toString());
+                        case "title" -> employee.setTitle(stringBuilder.toString());
+                        case "salary" -> employee.setSalary(Double.valueOf(stringBuilder.toString()));
+                        case "editUserRight" -> employee.setEditUserRight(Boolean.valueOf(stringBuilder.toString()));
+                        case "editBuildingRight" -> employee.setEditBuildingRight(Boolean.
+                                valueOf(stringBuilder.toString()));
+                        case "totalPrice" -> buildingOrder.setTotalPrice(Double.valueOf(stringBuilder.toString()));
+                        case "userManager" -> employee.setUser(user);
                     }
                 }
             }
         } catch (XMLStreamException | FileNotFoundException e) {
             e.printStackTrace();
         }
-        System.out.println(building);
+        System.out.println(client);
     }
 }
